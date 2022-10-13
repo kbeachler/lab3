d2adae68b7f96a259bb278b79bfb2da752367a4d
kbeachler <kaleigh8b@gmail.com>
2022-10-12 23:05:29 -0700

    fix

--- LinkedListTests.java
+++ LinkedListTests.java
@@ -1,10 +1,25 @@
-/*
 import static org.junit.Assert.*;
 import org.junit.*;
 
-class LinkedListTests{
+public class LinkedListTests{
 
-    
+    @Test
+    public void testPrepend(){
+        LinkedList temp = new LinkedList();
+        temp.prepend(2);
+        temp.prepend(3);
+        temp.prepend(-2);
 
+        assertEquals(3, temp.length());
+    }
+
+    @Test
+    public void testAppend(){
+        LinkedList temp = new LinkedList();
+        temp.append(2);
+        temp.append(3);
+        temp.append(-2);
+
+        assertEquals(3, temp.length());
+    }
 }
-*/
